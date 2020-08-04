e789833da44935003218c3ab9374803e1883a7a8
kristophmatthews <krist.m@gmail.com>
2020-08-04 09:07:23 -0400

    models

--- dino_manager/models.py
+++ dino_manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Dinosaur(models.Model):
+    name = models.TextField(max_length=255)
+    height_in_m = models.IntegerField()
+    is_carnivore = models.BooleanField()
+

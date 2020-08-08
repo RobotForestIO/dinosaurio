cb36e84de9a7a4143a05e3b8996c13937f8da71b
kristophmatthews <krist.m@gmail.com>
2020-08-08 12:28:22 -0400

    ajax working

--- frontend/src/components/DinosaurList.js
+++ frontend/src/components/DinosaurList.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class DinosaurList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dinosaurs: [],
+        };
+    }
+    componentDidMount() {
+        // this._getDinosaurs();
+        fetch('/api/v1/dinosaurs/')
+            .then(res => res.json())
+            .then(dinosaurs => {
+                this.setState({dinosaurs});
+            }, err => {
+                console.error(err);
+            });
+    }
+
     render() {
+        const {dinosaurs} = this.state;
         return (
             <div>
                 <h1>Dino List</h1>
+                {
+                    dinosaurs.length > 0 &&
+                        <ul>
+                            {
+                                dinosaurs.map((dinosaur, i) => {
+                                    return(
+                                        <li key={i}><Link to={`/dinosaurs/${dinosaur.id}`}>{dinosaur.name}</Link></li>
+                                    )
+                                })
+                            }
+                        </ul>
+                }
             </div>
         )
     }
